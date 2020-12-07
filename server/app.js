aeb5c8d5c4fd1eb64cece0112dc2f8dd140225ae
Leo Policastro <lpolicastro@pm.me>
2020-12-07 15:18:48 -0500

    set up login and create user routes and controllers

--- server/app.js
+++ server/app.js
@@ -1,11 +1,35 @@
 require('./db/config');
 const express = require('express'),
   morgan = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  openRoutes = require('./routes/open'),
+  path = require('path'),
   app = express();
 
 //Parse incoming JSON into objects
 app.use(express.json());
 // Log all requests
 app.use(morgan('dev'));
+
+// Unauthenticated routes
+app.use('/api/users', openRoutes);
+
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+}
+
+// We'll add more stuff in between in a little bit.
+
+if (process.env.NODE_ENV === 'production') {
+  // Handle React routing, return all requests to React app
+  app.get('*', (request, response) => {
+    response.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+    );
+  });
+}
 
 module.exports = app;
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -24,3 +24,24 @@ exports.createUser = async (req, res) => {
     res.status(400).json({ error: e.toString() });
   }
 };
+
+/**
+ * @param {email, password}
+ * Login a user
+ * @return {user}
+ */
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findByCredentials(email, password);
+    const token = await user.generateAuthToken();
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      sameSite: 'Strict',
+      secure: process.env.NODE_ENV !== 'production' ? false : true,
+    });
+    res.json(user);
+  } catch (e) {
+    res.status(400).json({ error: e.toString() });
+  }
+};

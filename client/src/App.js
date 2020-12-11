45fb861f7ee39dfa1e62cda0a7ca244fa1509f59
Cece Claridge <cece.claridge@gmail.com>
2020-12-11 15:43:58 -0500

    add signup and login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import { AppContextProvider } from './context/AppContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/signup' component={Signup} />
-      </Switch>
-    </BrowserRouter>
+    <AppContextProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={Signup} />
+        </Switch>
+      </BrowserRouter>
+    </AppContextProvider>
   );
 }
 

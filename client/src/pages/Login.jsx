4b5d9c5cd4856516f5cabff7e1960ba844f7c59e
Cece Claridge <cece.claridge@gmail.com>
2020-12-11 14:30:49 -0500

    add handle change to signup an login compponents

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [formData, setFormData] = useState(null);
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <Container className='container d-flex flex-column align-items-center justify-content-center fullscreen'>
       <h1>Task Manager</h1>
@@ -14,15 +20,9 @@ const Login = () => {
             type='email'
             placeholder='Email Address'
             name='email'
+            onChange={handleChange}
           />
         </Form.Group>
-        {/* <label htmlFor='password'>Password</label>
-        <input
-          type='password'
-          id='password'
-          name='password'
-          placeholder='Password'
-        /> */}
         <Form.Group>
           <Form.Label htmlFor='password'>Password</Form.Label>
           <Form.Control
@@ -30,13 +30,14 @@ const Login = () => {
             type='password'
             placeholder='Password'
             name='password'
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Group className='d-flex justify-content-center'>
           <Button type='submit'>Login</Button>
         </Form.Group>
       </Form>
-      <Link classname='mt-4' to='/signup'>
+      <Link className='mt-4' to='/signup'>
         Need an account? Sign up.
       </Link>
     </Container>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
+  const [formData, setFormData] = useState(null);
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <Container className='container d-flex flex-column align-items-center justify-content-center fullscreen'>
       <h1>Task Manager</h1>
@@ -14,6 +20,7 @@ const Signup = () => {
             type='text'
             placeholder='Full Name'
             name='name'
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Group>
@@ -23,6 +30,7 @@ const Signup = () => {
             type='email'
             placeholder='Email Address'
             name='email'
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Group>
@@ -32,13 +40,14 @@ const Signup = () => {
             type='password'
             placeholder='Password'
             name='password'
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Group className='d-flex justify-content-center'>
           <Button type='submit'>Login</Button>
         </Form.Group>
       </Form>
-      <Link classname='mt-4' to='/login'>
+      <Link className='mt-4' to='/login'>
         Already have an account? Log in.
       </Link>
     </Container>

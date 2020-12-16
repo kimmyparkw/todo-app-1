a1a946d19726712d23ca53f393c2027ed0fc8679
Cece Claridge <cece@wyncode.co>
2020-12-16 15:28:38 -0500

    profile page avatar upload, forgot password, and reset password functionality complete

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,72 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Container, Image, Button } from 'react-bootstrap';
 import wyncode from '../assets/images/wyncode.png';
+import Navigation from '../components/Navigation';
+import { AppContext } from '../context/AppContext';
+import axios from 'axios';
 
-const Profile = () => {
+const Profile = ({ history: { push } }) => {
+  const { currentUser, setCurrentUser, setLoading } = useContext(AppContext);
+
+  const [image, setImage] = useState(null);
+  const [preview, setPreview] = useState(null);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const avatar = new FormData();
+    avatar.append('avatar', image, image.name);
+
+    try {
+      const updatedUser = await axios({
+        method: 'POST',
+        url: '/api/users/avatar',
+        data: avatar,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      setCurrentUser({ ...currentUser, avatar: updatedUser.data.secureURL });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleImageSelect = e => {
+    setPreview(URL.createObjectURL(e.target.files[0]));
+    setImage(e.target.files[0]);
+  };
+
+  const handleDelete = async e => {
+    setLoading(true);
+    try {
+      await axios({
+        method: 'DELETE',
+        url: '/api/users',
+        withCredentials: true,
+      });
+      setLoading(false);
+      sessionStorage.removeItem('user');
+      setCurrentUser(null);
+      push('/login');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
+      <Navigation />
       <Container className='d-flex justify-content-center align-items-center flex-column'>
         <h1 className='mt-4'>Your Profile</h1>
         <div className='mt-4'>
           <Image
-            src={wyncode}
+            src={
+              preview
+                ? preview
+                : currentUser?.avatar
+                ? currentUser.avatar
+                : wyncode
+            }
             alt='profile-picture'
             width={250}
             height={250}
@@ -17,8 +74,8 @@ const Profile = () => {
           />
         </div>
         <div className='mt-4'>
-          <form className='d-flex flex-column'>
-            <input type='file' accept='image/*' />
+          <form className='d-flex flex-column' onSubmit={handleSubmit}>
+            <input type='file' accept='image/*' onChange={handleImageSelect} />
             <Button type='submit' size='sm' className='mt-4'>
               Save Image
             </Button>
@@ -29,15 +86,17 @@ const Profile = () => {
             <label htmlFor='name' className='pr-4 font-weight-bold'>
               Name:
             </label>
-            <p>User name goes here.</p>
+            <p>{currentUser?.name}</p>
           </div>
           <div className='d-flex'>
             <label htmlFor='email' className='pr-4 font-weight-bold'>
               Email:
             </label>
-            <p>User email goes here</p>
+            <p>{currentUser?.email}</p>
           </div>
-          <Button variant='danger'>Delete Account</Button>
+          <Button variant='danger' onClick={handleDelete}>
+            Delete Account
+          </Button>
         </div>
       </Container>
     </>
